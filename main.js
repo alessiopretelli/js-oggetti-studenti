90a8e7ae53c4c72349bb62d2161b455bf0e4f9d9
Alessio <ale79631@gmail.com>
2021-03-10 13:38:57 +0100

    fine

---
+++ main.js
@@ -0,0 +1,49 @@
+function letturaStudenti(alunni) {
+    for (i = 0; i < alunni.length; i++) {
+        console.log(alunni[i].nome + ' ' + alunni[i].cognome);
+    }    
+}
+
+function nuovoStudente(a) {
+    a[a.length] = {};
+    a[a.length - 1]['nome'] = prompt("Inserisci il tuo nome.");
+    a[a.length - 1]['cognome'] = prompt("Inserisci il tuo cognome.");
+    a[a.length - 1]['eta'] = prompt("Inserisci la tua eta'.") * 1;
+    
+    return a;
+}
+
+var studente = {
+    'nome': 'Alessio',
+    'cognome': 'Pretelli',
+    'eta': 24
+};
+
+for (var key in studente) {
+    console.log(key + ': ' + studente[key]);
+}
+
+var studenti = [
+    {
+        'nome': 'Alessio',
+        'cognome': 'Pretelli',
+        'eta': 24
+    },
+    {
+        'nome': 'Genoveffo',
+        'cognome': 'Bracciacorte',
+        'eta': 58
+    },
+    {
+        'nome': 'Sauro',
+        'cognome': 'Dino',
+        'eta': '25000000~'
+    },
+];
+
+letturaStudenti(studenti);
+
+studenti = nuovoStudente(studenti);
+
+letturaStudenti(studenti);
+console.log(studenti);
