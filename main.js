e85aefb7a35daad58135aa0285461a6c9bd9bf73
Alessio <ale79631@gmail.com>
2021-03-10 18:27:47 +0100

    inserimento dati con push

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ function letturaStudenti(alunni) {
 }
 
 function nuovoStudente(a) {
+    /*
     a[a.length] = {};
     a[a.length - 1]['nome'] = prompt("Inserisci il tuo nome.");
     a[a.length - 1]['cognome'] = prompt("Inserisci il tuo cognome.");
     a[a.length - 1]['eta'] = prompt("Inserisci la tua eta'.") * 1;
-    
+    */
+    //alternativa
+    a.push({
+        ['nome'] : prompt("Inserisci il tuo nome."), 
+        ['cognome'] : prompt("Inserisci il tuo cognome."), 
+        ['eta'] : prompt("Inserisci la tua eta'.") * 1
+    });
+
     return a;
 }
 
